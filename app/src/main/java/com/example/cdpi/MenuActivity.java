77d620bc22434e921a8909c159a152d031c00618
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-05-28 16:40:57 +0200

    Click sur Etoile

--- app/src/main/java/com/example/cdpi/MenuActivity.java
+++ app/src/main/java/com/example/cdpi/MenuActivity.java
@@ -2,7 +2,9 @@ package com.example.cdpi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void launchRateActivity(View v){
+        Intent i = new Intent(this,RateActivity.class);
+        startActivity(i);
     }
 }
