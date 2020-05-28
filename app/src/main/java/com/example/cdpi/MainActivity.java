fd0bf0f19fadcb9cc2274415fe2137dd89232e69
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-05-28 16:23:18 +0200

    Animation

--- app/src/main/java/com/example/cdpi/MainActivity.java
+++ app/src/main/java/com/example/cdpi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cdpi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.gson.Gson;
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Animator.AnimatorListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +25,42 @@ public class MainActivity extends AppCompatActivity {
     public void connect(View v){
         EditText loginText = findViewById(R.id.main_login);
         EditText passwordText = findViewById(R.id.main_password);
-        Intent i = new Intent(this,MenuActivity.class);
+
 
         if (    "g".equals(loginText.getText().toString())
-            &&  "p".equals(passwordText.getText().toString()))
-            startActivity(i);
+            &&  "p".equals(passwordText.getText().toString())) {
+
+            View main = findViewById(R.id.main_layout);
+            main.animate()
+                    .alpha(0.0f)
+                    .translationX(200)
+                    .setDuration(400)
+                    .setListener(this);
+
+
+        }
         else Toast.makeText(this,R.string.message_error_connect,Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        Intent i = new Intent(this,MenuActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
 
     }
 }
